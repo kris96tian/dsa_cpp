b140fa51df4972b0f6d254650a337267941e5af5
Kristian Alikaj <92834350+kris96tian@users.noreply.github.com>
2024-04-04 16:45:46 +0200

    Update horspool_main.cpp
    Arg[1] can be also in fasta format

--- HORSPOOL/horspool_main.cpp
+++ HORSPOOL/horspool_main.cpp
@@ -1,26 +1,50 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <vector>
 #include "horspool.h"
 
-int main(int argc, const char* argv[])
-{
-  if (argc != 3)
-  {
-    std::cout << argv[0] << " <TEXT> <PATTERN>" << std::endl;
-    return 1;
-  }
-
-  Horspool h;
-  h.setPattern(argv[2]);
-  auto hits = h.getHits(argv[1]); 
-  std::cout << "Horspool search:\n\n-- Text is: --\n" << argv[1] << "\n-- Hits: --\n";
-    
-  for (const auto hit : hits)
-  {
-    std::cout << std::string(hit, ' ') << argv[2] << "\n";      
-  }
-  
-  return 0;
+std::string readFasta(const std::string& filename) {
+    std::ifstream file(filename);
+    std::string sequence;
+    std::string line;
+    while (std::getline(file, line)) {
+        if (line.empty() || line[0] == '>') {
+            continue;
+        }
+        sequence += line;
+    }
+    return sequence;
 }
 
+int main(int argc, const char* argv[]) {
+    if (argc != 3) {
+        std::cout << argv[0] << " <TEXT_OR_FASTA_FILE> <PATTERN>" << std::endl;
+        return 1;
+    }
+
+    std::string sequence;
+    std::string filename = argv[1];
+    if (filename.size() >= 5 && (filename.substr(filename.size() - 5) == ".fasta" || filename.substr(filename.size() - 3) == ".fa")) {
+        // Argument is a FASTA file
+        sequence = readFasta(filename);
+        if (sequence.empty()) {
+            std::cerr << "Error: Failed to read sequence from file: " << filename << std::endl;
+            return 1;
+        }
+    } else {
+        // Argument is a string
+        sequence = filename;
+    }
+
+    Horspool h;
+    h.setPattern(argv[2]);
+    auto hits = h.getHitsFromSequence(sequence);
+    
+    std::cout << "Horspool search:\n\n-- Hits: --\n";
+    for (const auto hit : hits) {
+        std::cout << "Pattern found at position: " << hit << std::endl;      
+    }
+  
+    return 0;
+}
